f3b6fb5fd5e9f17ce13e0fe39580af4f3eb8d469
GVitSer <guvitsvst@mail.ru>
2025-03-26 19:49:59 +0300

    Решение задачи 02-javascript-data-types/1-sort-strings

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,18 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
+  const sortArr = [...arr];
 
+  sortArr.sort((a, b) => {
+    const sortParam = {
+      caseFirst: 'upper',
+      locales: ['ru', 'en']
+    };
+
+    return param === 'asc'
+      ? a.localeCompare(b, sortParam.locales, { caseFirst: sortParam.caseFirst })
+      : b.localeCompare(a, sortParam.locales, { caseFirst: sortParam.caseFirst });
+  });
+
+  return sortArr;
 }
