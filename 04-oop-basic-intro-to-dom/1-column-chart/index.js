1b0488eebaac944d9b3f6d08425f73b350ff50df
GVitSer <guvitsvst@mail.ru>
2025-04-03 18:43:21 +0300

    Решение задачи 04-oop-basic-intro-to-dom

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -62,23 +62,6 @@ export default class ColumnChart {
         </div>
         <div data-element="body" class="column-chart__chart">
         ${this.createChartTemplate()}
-
-
-          <!--<div style="&#45;&#45;value: 16" data-tooltip="33%"></div>
-          <div style="&#45;&#45;value: 8" data-tooltip="17%"></div>
-          <div style="&#45;&#45;value: 33" data-tooltip="67%"></div>
-          <div style="&#45;&#45;value: 14" data-tooltip="29%"></div>
-          <div style="&#45;&#45;value: 6" data-tooltip="13%"></div>
-          <div style="&#45;&#45;value: 25" data-tooltip="50%"></div>
-          <div style="&#45;&#45;value: 8" data-tooltip="17%"></div>
-          <div style="&#45;&#45;value: 6" data-tooltip="13%"></div>
-          <div style="&#45;&#45;value: 18" data-tooltip="38%"></div>
-          <div style="&#45;&#45;value: 50" data-tooltip="100%"></div>
-          <div style="&#45;&#45;value: 41" data-tooltip="83%"></div>
-          <div style="&#45;&#45;value: 35" data-tooltip="71%"></div>
-          <div style="&#45;&#45;value: 41" data-tooltip="83%"></div>
-          <div style="&#45;&#45;value: 4" data-tooltip="8%"></div>
-          <div style="&#45;&#45;value: 2" data-tooltip="4%"></div>-->
         </div>
       </div>
     </div>
